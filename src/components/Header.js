ceec6ddd2eb40ec9058061a7e5ba42c39bfc6381
Sharon <sharonrachelg@gmail.com>
2025-04-12 17:46:32 -0400

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,7 +7,7 @@ const Header = () => (
     <div className='nav'>
       <div className='nav-items'>
         <h1 className='nav-title'><FontAwesomeIcon className='nav-title_icon' icon={faCloudSun} />Can I Wear a Hoodie Today?</h1>
-        <a href='https://github.com/shaerins' target='_blank' rel='noopener noreferrer'><FontAwesomeIcon icon={faCode} /></a>
+        <a href='https://github.com/sharonrcg' target='_blank' rel='noopener noreferrer'><FontAwesomeIcon icon={faCode} /></a>
       </div>
     </div>
   </header>
